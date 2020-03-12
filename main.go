98ac9765806f9df74541078f5035b22396212bf3
Ian Macalinao <me@ian.pw>
2020-03-11 19:09:03 -0500

    Get the url of a post

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/turnage/graw/reddit"
+)
 
 func main() {
-	fmt.Println("hello world")
+	script, err := reddit.NewScript("shui v0.1", 2*time.Second)
+	if err != nil {
+		panic(err)
+	}
+
+	harvest, err := script.ListingWithParams("/r/hydrohomies/top", map[string]string{"t": "day"})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print(harvest.Posts[0].URL)
 }
